22b55330d273f227448762a86a2b968f6391f8e6
JFFleming <84772413+JFFleming@users.noreply.github.com>
2026-05-23 15:25:02 +0200

    Delete ObtainSignificantHits.sh

--- ObtainSignificantHits.sh
+++
@@ -1,9 +0,0 @@
-while read i; do
-  count=$(grep "$i" */Test_Table.tsv | wc -l)
-  printf "%s\t%s\n" "${i//\"/}" "$count"
-done < Unique.txt > Unique.Appearances.tsv
-
-while read i; do
-  count=$(grep "$i.*yes" */Test_Table.tsv | wc -l)
-  printf "%s\t%s\n" "${i//\"/}" "$count"
-done < Unique.txt > Unique.Significant.tsv
